cc89c69ca095f02861f08e01eafc652bec89c957
Jimmy Rutsfrom:(noreply@github.com)Rutsfrom:(noreply@github.com) <jimmy.ruts@thomascookonline.com>
2016-04-19 16:29:29 +0100

    Expecting the right format instead of parsing it here

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const awsHelper = require('aws-lambda-helper');
-const tile_mapper = require('./lib/tile_mapper');
 const dynamo_insert = require('./lib/dynamo_insert');
 
 exports.handler = function (event, context) {
   console.log('Received event:', JSON.stringify(event, null, 2)); // debug SNS
   const message = event.Records[0].Sns.Message;
   const bucket_id = message.id;
-  const tiles = tile_mapper(message.query.tiles);
+  const tiles = message.content.tiles;
   if (tiles) {
     awsHelper.init(context);
     dynamo_insert(bucket_id, tiles, function (err, data) {
