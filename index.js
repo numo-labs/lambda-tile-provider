6027d71c994896d9745f10cccb39448d99ac7189
Jimmy Rutsfrom:(noreply@github.com)Rutsfrom:(noreply@github.com) <jimmy.ruts@thomascookonline.com>
2016-05-11 15:04:36 +0100

    Added better logging

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const content_handler = require('./lib/content_handler');
 const dynamo_insert = require('./lib/dynamo_insert');
 
 exports.handler = function (event, context, callback) {
-  awsHelper.init(context);
-  console.log('Received event:', JSON.stringify(event, null, 2)); // debug SNS
+  awsHelper.init(context, event);
+  awsHelper.Logger('lambda-tile-provider');
+  awsHelper.log.info({ event: event }, 'Incoming event');
   const message = JSON.parse(event.Records[0].Sns.Message);
   const bucket_id = message.id;
   const tiles = message.data.content.tiles;
